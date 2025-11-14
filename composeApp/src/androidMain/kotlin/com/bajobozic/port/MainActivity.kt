99606eb9e8d2384e1b4a297964674801857f3333
Savo Bozic <bajobozic@gmail.com>
2025-11-14 10:15:43 +0100

    Enable edge-to-edge display with dark status bar in MainActivity

--- composeApp/src/androidMain/kotlin/com/bajobozic/port/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/bajobozic/port/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.bajobozic.port
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.graphics.toColorInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        enableEdgeToEdge(statusBarStyle = SystemBarStyle.dark("#801b1b1b".toColorInt()))
         super.onCreate(savedInstanceState)
         setContent {
             App()
