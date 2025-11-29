e1ed5927fb48ebbebebbdf514ac670f7e2f81510
Savo Bozic <bajobozic@gmail.com>
2025-11-29 14:54:08 +0100

    Refactor MovieRemoteMediator to enhance error handling and load key determination for improved robustness

--- composeApp/src/commonMain/kotlin/com/bajobozic/port/home_component/data/mediator/MovieRemoteMediator.kt
+++ composeApp/src/commonMain/kotlin/com/bajobozic/port/home_component/data/mediator/MovieRemoteMediator.kt
@@ -8,6 +8,7 @@ import com.bajobozic.port.network.domain.model.MovieDetail
 import com.bajobozic.port.network.domain.model.initRemoteKeys
 import com.bajobozic.port.network.domain.usecase.GetGenresUseCase
 import com.bajobozic.port.network.domain.usecase.GetMoviesUseCase
+import com.bajobozic.port.shared_component.domain.BaseError
 import com.bajobozic.port.shared_component.domain.onError
 import com.bajobozic.port.shared_component.domain.onSuccess
 import com.bajobozic.port.storage.domain.model.Genre
@@ -37,7 +38,7 @@ internal class MovieRemoteMediator(
     ): MediatorResult {
         return try {
             val loadKey = when (loadType) {
-                LoadType.REFRESH -> state.anchorPosition ?: 1
+                LoadType.REFRESH -> if (state.pages.isNotEmpty()) state.pages.size + 1 else 1
                 LoadType.PREPEND -> return MediatorResult.Success(endOfPaginationReached = true)
                 LoadType.APPEND -> getMaxCurrentPageUseCase() + 1
             }
@@ -50,7 +51,11 @@ internal class MovieRemoteMediator(
                 val gr = grd.await()
                 mr.onSuccess { movies ->
                     moviesResponse = movies.initRemoteKeys().movies
-                }.onError { _ -> emptyList<MovieDetail>() }
+                }.onError { error ->
+                    //if there is error in network layer or there is no internet connection,
+                    //we must rethrow exception here to avoid clearing database and instead show data from database,
+                    throw RuntimeException(getError(error))
+                }
                 genreResponse = gr
             }
             val genreIdsPerMovie = moviesResponse.map { it.genreIds }
@@ -97,5 +102,19 @@ internal class MovieRemoteMediator(
             genresList = emptyList<Genre>()
         }
         return genresList
+    }
+
+    private fun getError(error: BaseError): String {
+        return when (error) {
+            is BaseError.ApiError.HttpServerError -> error.message
+            BaseError.ApiError.NoInternet -> "Check internet connection"
+            BaseError.ApiError.ToManyRequest -> "To many requests"
+            is BaseError.ApiError.UnknownError -> error.message
+            is BaseError.FileError.UnknownError -> error.message
+            BaseError.FileError.IllegalArgumentException -> "Invalid param"
+            BaseError.FileError.IllegalStateException -> "Invalid state"
+            BaseError.FileError.RuntimeException -> " Something went wrong"
+            BaseError.FileError.SQLiteException -> "SQLite error"
+        }
     }
 }
