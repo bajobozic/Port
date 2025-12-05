f8ba038c64556461efa7a44718497fdf28a7087e
Savo Bozic <bajobozic@gmail.com>
2025-12-05 11:15:26 +0100

    Add additional plugin dependencies for Kotlin serialization, KSP, Room, and Google services

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,8 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.kotlinSerialization) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.androidx.room) apply false
+    alias(libs.plugins.google.gms) apply false
 }
