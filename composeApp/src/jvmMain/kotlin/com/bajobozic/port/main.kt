efaaf69122b6eac6e72a7e6d3c3fccbb791a2d56
Savo Bozic <bajobozic@gmail.com>
2026-01-05 11:23:25 +0100

    Change initial size of jvm window app

--- composeApp/src/jvmMain/kotlin/com/bajobozic/port/main.kt
+++ composeApp/src/jvmMain/kotlin/com/bajobozic/port/main.kt
@@ -1,13 +1,20 @@
 package com.bajobozic.port
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
     initKoin()
+    val windowState = rememberWindowState(
+        width = 1280.dp,
+        height = 720.dp
+    )
     Window(
         onCloseRequest = ::exitApplication,
         title = "Port",
+        state = windowState
     ) {
         App()
     }
