fdaac851c90aa2e434a1aa5e6243c60dfffc0ba2
Savo Bozic <bajobozic@gmail.com>
2026-01-08 17:45:30 +0100

    Set java to 21

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -70,7 +70,7 @@ val generateConfig by tasks.registering(GenerateConfigTask::class) {
 kotlin {
     androidTarget {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
 
