d53edf63a9a8e22903760a4d4921f44b6a401f42
Savo Bozic <bajobozic@gmail.com>
2026-01-09 12:06:13 +0100

    Unifies MinSdk and compile to all modules

--- signin_ui/build.gradle.kts
+++ signin_ui/build.gradle.kts
@@ -13,8 +13,8 @@ kotlin {
     // See: https://kotlinlang.org/docs/multiplatform-discover-project.html#targets
     androidLibrary {
         namespace = "com.bajobozic.signin_ui"
-        compileSdk = 36
-        minSdk = 24
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
 
         withHostTestBuilder {
         }
