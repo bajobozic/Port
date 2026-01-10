8baaf7878171a39b68612cb8e4bc8819087fd5b4
Savo Bozic <bajobozic@gmail.com>
2026-01-10 17:57:01 +0100

    Add notification support for JVM

--- composeApp/src/jvmMain/kotlin/com/bajobozic/port/main.kt
+++ composeApp/src/jvmMain/kotlin/com/bajobozic/port/main.kt
@@ -4,6 +4,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import com.mmk.kmpnotifier.extensions.composeDesktopResourcesPath
+import com.mmk.kmpnotifier.notification.NotifierManager
+import com.mmk.kmpnotifier.notification.configuration.NotificationPlatformConfiguration
+import java.io.File
 
 fun main() = application {
     initKoin()
@@ -11,6 +15,13 @@ fun main() = application {
         width = 1280.dp,
         height = 720.dp
     )
+    NotifierManager.initialize(
+        NotificationPlatformConfiguration.Desktop(
+            showPushNotification = true,
+            notificationIconPath = composeDesktopResourcesPath() + File.separator + "ic_notification.png"
+        )
+    )
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "Port",
