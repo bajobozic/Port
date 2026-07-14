dc4be49ef53c80ee51d9c8d413decbadb77203b0
Savo Bozic <bajobozic@gmail.com>
2026-07-14 23:59:45 +0200

    Update swift packages

--- iosApp/KotlinMultiplatformLinkedPackage/Package.swift
+++ iosApp/KotlinMultiplatformLinkedPackage/Package.swift
@@ -1,28 +1,26 @@
 // swift-tools-version: 5.9
-
 import PackageDescription
-
 let package = Package(
     name: "KotlinMultiplatformLinkedPackage",
     platforms: [
-        .iOS("16.0")
+      .iOS("16.0")
     ],
     products: [
-        .library(
-            name: "KotlinMultiplatformLinkedPackage",
-            type: .none,
-            targets: ["KotlinMultiplatformLinkedPackage"]
-        )
+      .library(
+          name: "KotlinMultiplatformLinkedPackage",
+          type: .none,
+          targets: ["KotlinMultiplatformLinkedPackage"]
+      )
     ],
     dependencies: [
-        .package(path: "subpackages/io_github_mirzemehdi_kmpnotifier_push_firebase_2_0_0")
+      .package(path: "subpackages/io_github_mirzemehdi_kmpnotifier_push_firebase_2_0_0")
     ],
     targets: [
-        .target(
-            name: "KotlinMultiplatformLinkedPackage",
-            dependencies: [
-                .product(name: "io_github_mirzemehdi_kmpnotifier_push_firebase_2_0_0", package: "io_github_mirzemehdi_kmpnotifier_push_firebase_2_0_0")
-            ]
-        )
+      .target(
+          name: "KotlinMultiplatformLinkedPackage",
+          dependencies: [
+            .product(name: "io_github_mirzemehdi_kmpnotifier_push_firebase_2_0_0", package: "io_github_mirzemehdi_kmpnotifier_push_firebase_2_0_0")
+          ]
+      )
     ]
 )
--- iosApp/KotlinMultiplatformLinkedPackage/subpackages/io_github_mirzemehdi_kmpnotifier_push_firebase_2_0_0/Package.swift
+++ iosApp/KotlinMultiplatformLinkedPackage/subpackages/io_github_mirzemehdi_kmpnotifier_push_firebase_2_0_0/Package.swift
@@ -1,7 +1,5 @@
 // swift-tools-version: 5.9
-
 import PackageDescription
-
 let package = Package(
     name: "io_github_mirzemehdi_kmpnotifier_push_firebase_2_0_0",
     platforms: [
